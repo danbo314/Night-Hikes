6de1ca2178a56b2897d5d79856e6a1ff24eb9f54
Daniel Bolivar <dan@tao.local>
2016-07-03 20:18:16 -0400

    test collage plus

---
+++ photos.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dan on 7/3/16.
+ */
+
+$(function () {
+
+    $("#collage").removeWhitespace().collagePlus({
+        effect: "effect-2",
+        allowPartialLastRow : true,
+        "targetHeight": 300
+    });
+
+});
