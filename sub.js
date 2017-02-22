b66e0519c68636f58ab755ecd235989fec3ca6e0
Daniel Bolivar <dan@tao.local>
2017-02-22 00:57:10 -0600

    adding press kit page, some minor style fixes, add souncloud on music page + youtube embed

--- sub.js
+++ sub.js
@@ -6,4 +6,35 @@ $(function () {
     $('.icon-menu').click(function () {
         $('#nav-menu').toggle();
     });
+
+    $('#accordion').on('click', '.title', function () {
+        $('#accordion').find('.selected').removeClass('selected');
+        $('#accordion') .find('.content').css({ height: 0 });
+
+        var newH;
+
+        switch($(this).parent('li').attr('id')) {
+            case 'liner':
+                newH = 300;
+                break;
+            case 'album':
+                newH = 520;
+                break;
+            case 'bio':
+                newH = 500;
+                break;
+            case 'photos':
+                newH = 1300;
+                break;
+            case 'youtube':
+                newH = 520;
+                break;
+            default:
+                newH = 200;
+                break;
+        }
+
+        $(this).addClass('selected');
+        $(this).siblings('.content').css({ height: newH });
+    });
 });
